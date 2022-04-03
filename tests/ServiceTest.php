b1ba7e4d7750da68b9526cc01a86bfea4708d865
emils <emilsaditajs@gmail.com>
2022-04-03 14:45:48 +0300

    step three tests

--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -16,7 +16,7 @@ class ServiceTest extends TestCase
     public function testIfMultipleOfFiveIsBar(): void
     {
         $service = new Service();
-        $this->assertSame('Bar', $service->checkNumber(5));
+        $this->assertSame('Bar', $service->checkNumber(10));
     }
 
     public function testIfMultipleOfSevenIsQix(): void
@@ -28,13 +28,13 @@ class ServiceTest extends TestCase
     public function testIfSeveralMultiplesIsFooBar(): void
     {
         $service = new Service();
-        $this->assertSame('FooBar', $service->checkNumber(15));
+        $this->assertSame('FooBar', $service->checkNumber(60));
     }
 
     public function testIfSeveralMultiplesIsBarQix(): void
     {
         $service = new Service();
-        $this->assertSame('BarQix', $service->checkNumber(35));
+        $this->assertSame('BarQix', $service->checkNumber(140));
     }
 
     public function testIfSeveralMultiplesIsFooQix(): void
@@ -46,10 +46,103 @@ class ServiceTest extends TestCase
     public function testIfSeveralMultiplesIsFooBarQix(): void
     {
         $service = new Service();
-        $this->assertSame('FooBarQix', $service->checkNumber(105));
+        $this->assertSame('FooBarQix', $service->checkNumber(210));
     }
 
-    public function testIfNoMultiplesReturnString(): void
+
+
+
+    public function testIfOccurrenceOfThreeIsFoo(): void
+    {
+        $service = new Service();
+        $this->assertSame('Foo', $service->checkNumber(31));
+    }
+
+    public function testIfOccurrenceOfFiveIsBar(): void
+    {
+        $service = new Service();
+        $this->assertSame('Bar', $service->checkNumber(51));
+    }
+
+    public function testIfOccurrenceOfSevenIsQix(): void
+    {
+        $service = new Service();
+        $this->assertSame('Qix', $service->checkNumber(71));
+    }
+
+    public function testIfSeveralOccurrencesIsFooBar(): void
+    {
+        $service = new Service();
+        $this->assertSame('FooBar', $service->checkNumber(352));
+    }
+
+    public function testIfSeveralOccurrencesIsBarQix(): void
+    {
+        $service = new Service();
+        $this->assertSame('BarQix', $service->checkNumber(572));
+    }
+
+    public function testIfSeveralOccurrencesIsFooQix(): void
+    {
+        $service = new Service();
+        $this->assertSame('FooQix', $service->checkNumber(374));
+    }
+
+    public function testIfSeveralOccurrencesIsFooBarQix(): void
+    {
+        $service = new Service();
+        $this->assertSame('FooBarQix', $service->checkNumber(3572));
+    }
+
+
+
+
+    public function testIfOccurrenceAndMultipleOfThreeIsFooFoo(): void
+    {
+        $service = new Service();
+        $this->assertSame('FooFoo', $service->checkNumber(30));
+    }
+
+    public function testIfOccurrenceAndMultipleOfFiveIsBarBar(): void
+    {
+        $service = new Service();
+        $this->assertSame('BarBar', $service->checkNumber(50));
+    }
+
+    public function testIfOccurrenceAndMultipleOfSevenIsQixQix(): void
+    {
+        $service = new Service();
+        $this->assertSame('QixQix', $service->checkNumber(70));
+    }
+
+    public function testIfSeveralOccurrencesAndMultiplesIsFooBarFooBar(): void
+    {
+        $service = new Service();
+        $this->assertSame('FooBarFooBar', $service->checkNumber(135));
+    }
+
+    public function testIfSeveralOccurrencesAndMultiplesIsBarQixBarQix(): void
+    {
+        $service = new Service();
+        $this->assertSame('BarQixBarQix', $service->checkNumber(175));
+    }
+
+    public function testIfSeveralOccurrencesAndMultiplesIsFooQixFooQix(): void
+    {
+        $service = new Service();
+        $this->assertSame('FooQixFooQix', $service->checkNumber(373));
+    }
+
+    public function testIfSeveralOccurrencesAndMultiplesIsFooBarQixFooBarQix(): void
+    {
+        $service = new Service();
+        $this->assertSame('FooBarQixFooBarQixFooBarQixFooBarQix', $service->checkNumber(735));
+    }
+
+
+
+
+    public function testIfNoTransformationsReturnString(): void
     {
         $service = new Service();
         $this->assertSame('4', $service->checkNumber(4));
