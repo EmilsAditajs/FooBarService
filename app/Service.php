dbd0e3fdf6c6f759cef4fbefe267643768ec6028
emils <emilsaditajs@gmail.com>
2022-04-03 13:44:58 +0300

    step two complete

--- app/Service.php
+++ app/Service.php
@@ -6,24 +6,45 @@ class Service
 {
     private int $foo = 3;
     private int $bar = 5;
+    private int $qix = 7;
 
-    private function action(int $number1, int $number2): bool
+    public function action(int $number1, int $number2): bool
     {
         return $number1 % $number2 == 0;
     }
 
     public function checkNumber(int $input): string
     {
-        if($this->action($input, $this->foo) && $this->action($input, $this->bar)) {
+        $foo = $this->foo;
+        $bar = $this->bar;
+        $qix = $this->qix;
+
+        if ($this->action($input, $foo) && $this->action($input, $bar) && $this->action($input, $qix)) {
+            return 'FooBarQix';
+        }
+
+        if ($this->action($input, $foo) && $this->action($input, $bar)) {
             return 'FooBar';
         }
 
-        if($this->action($input, $this->foo)) {
+        if ($this->action($input, $foo) && $this->action($input, $qix)) {
+            return 'FooQix';
+        }
+
+        if ($this->action($input, $bar) && $this->action($input, $qix)) {
+            return 'BarQix';
+        }
+
+        if ($this->action($input, $foo)) {
             return 'Foo';
         }
 
-        if($this->action($input, $this->bar)) {
+        if ($this->action($input, $bar)) {
             return 'Bar';
+        }
+
+        if ($this->action($input, $qix)) {
+            return 'Qix';
         }
 
         return $input;
